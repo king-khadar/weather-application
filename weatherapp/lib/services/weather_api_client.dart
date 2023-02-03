5e6988140affbb2f3841a1fb71b656601e0cc6d3
c/khadar maxamed cali <114425755+king-khadar@users.noreply.github.com>
2023-02-03 10:51:36 +0300

    Update weather_api_client.dart

--- weatherapp/lib/services/weather_api_client.dart
+++ weatherapp/lib/services/weather_api_client.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:wheatherapp/model/weather_model.dart';
+import 'package:weatherapp/model/weather_model.dart';
 
 class WeatherApiClient {
   Future<Weather>? getCurrentWeather(String? location) async {
