bd87e26a07d54eb44dbe884a14452b430c8c09c0
khadar <maxaakafiirinee@gmail.com>
2023-02-03 09:00:07 +0300

    first

---
+++ weatherapp/lib/services/weather_api_client.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:wheatherapp/model/weather_model.dart';
+
+class WeatherApiClient {
+  Future<Weather>? getCurrentWeather(String? location) async {
+    var endpoint = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=7071a7dcbdd4b157a7307bd84670f32d&units=metrics");
+
+    var response = await http.get(endpoint);
+    var body = jsonDecode(response.body);
+    print(Weather.fromJson(body).cityName);
+    return Weather.fromJson(body);
+  }
+}
